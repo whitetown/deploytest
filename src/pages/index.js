c82befbfe7543c9362d3d0f343980b0ce559fac4
Sergey Chehuta <sc@whitetown.com>
2024-09-12 19:07:45 +0200

    initial

---
+++ src/pages/index.js
@@ -0,0 +1,16 @@
+
+
+export default function Home() {
+  return (
+      <main className="mx-auto max-w-3xl py-8">
+        <div className="grid grid-cols-4 gap-4">
+
+            <img src='/red.png' className="w-20 h-20 object-fill bg-gray-400" />
+            <img src='/green.png' className="w-20 h-20 object-fill bg-gray-400" />
+            <img src='/blue.png' className="w-20 h-20 object-fill bg-gray-400" />
+            <img src='/yellow.png' className="w-20 h-20 object-fill bg-gray-400" />
+
+        </div>
+      </main>
+  );
+}
---
+++ theme.sh
@@ -0,0 +1,6 @@
+#/bin/sh
+
+cp -f theme/black.png public/red.png
+cp -f theme/black.png public/green.png
+cp -f theme/black.png public/blue.png
+cp -f theme/black.png public/yellow.png
