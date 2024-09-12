e69a0c66f774ec18bbb2f00c3f5adb2f8d60c0a4
Sergey Chehuta <sc@whitetown.com>
2024-09-12 19:19:24 +0200

    log messages

--- theme.sh
+++ theme.sh
@@ -1,6 +1,11 @@
 #/bin/sh
 
+echo "copy black files to colorful files"
 cp -f theme/black.png public/red.png
 cp -f theme/black.png public/green.png
 cp -f theme/black.png public/blue.png
 cp -f theme/black.png public/yellow.png
+
+echo "all files below have to be equal"
+ls -la theme/*.png
+ls -la public/*.png
